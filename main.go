2eb8506152232fa8fb58fdf40538318c0c0a697e
Timmy Xiao <timmyxiao1@gmail.com>
2019-08-28 06:54:07 -0400

    Create basic chat bot

--- main.go
+++ main.go
@@ -1,9 +1,59 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "github.com/joho/godotenv"
+    "os"
+    "github.com/bwmarrin/discordgo"
+)
+
+var (
+    Token string
 )
 
 func main () {
-    fmt.Println("Hello World")
+    loadEnv()
+    newSession()
 }
+
+func loadEnv () {
+    env_err := godotenv.Load()
+    if env_err != nil {
+        log.Println("Error loading env file!")
+    }
+
+    Token = os.Getenv("TOKEN")
+}
+
+func newSession() {
+    dg, err := discordgo.New("Bot " + Token)
+    if err != nil {
+        log.Println(err)
+    }
+
+    dg.AddHandler(msgHandler)
+
+    //Open socket and listen
+    err = dg.Open()
+    if err != nil {
+        log.Println("error connecting," ,err)
+        return
+    }
+
+    // Uhh, this makes CTRL-C exit
+    <-make(chan struct{})
+    return
+}
+
+func msgHandler(s *discordgo.Session, m *discordgo.MessageCreate){
+    // Ignore messages made by self
+    if  m.Author.ID == s.State.User.ID {
+        return
+    }
+
+    if m.Content == "owo" {
+        s.ChannelMessageSend(m.ChannelID, "uwu")
+    }
+}
+
+
